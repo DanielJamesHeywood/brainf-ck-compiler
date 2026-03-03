61ff936607088c89298ad7b376d9d29174bbf548
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-03 15:33:53 +0000

    Declare a default-localization for our package.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
 // swift-tools-version: 6.2
 import PackageDescription
 
-let package = Package(name: "brainf*ck compiler")
+let package = Package(name: "brainf*ck compiler", defaultLocalization: "en-GB")
