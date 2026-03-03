8339fb2b5462b33dc26b3a56271495a19fb75d59
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-03 19:29:56 +0000

    Guard against an incorrect number of command line arguments

--- Sources/brainf*ck compiler/main.swift
+++ Sources/brainf*ck compiler/main.swift
@@ -1,3 +1,10 @@
 import Utilities
 
+guard CommandLine.arguments.count == 2 else {
+    print(
+        "Expected exactly 1 argument, but got \(CommandLine.arguments.count - 1)",
+        to: .standardError
+    )
+    exit(with: .failure)
+}
 exit(with: .failure)
