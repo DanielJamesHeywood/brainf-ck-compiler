00413a239c0a28bef845787142d86f6831cc3123
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-03 23:06:08 +0000

    Nest ExitTests within UtilitiesTests so we can test utilities as a whole

---
+++ Sources/Utilities tests/UtilitiesTests.swift
@@ -0,0 +1,27 @@
+import Testing
+import Utilities
+
+@Suite struct UtilitiesTests {
+    
+    @Suite struct ExitTests {
+        
+        @Test func exitingWithSuccessExitsWithSuccess() async {
+            await #expect(
+                processExitsWith: .success,
+                performing: {
+                    exit(with: .success)
+                }
+            )
+        }
+        
+        @Test func exitingWithFailureExitsWithFailure() async {
+            await #expect(
+                processExitsWith: .failure,
+                performing: {
+                    exit(with: .failure)
+                }
+            )
+        }
+    }
+
+}
