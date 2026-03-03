19d13528ac6f2b94fbf725e07a63bf5b034efc83
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-03 15:58:24 +0000

    Declare an executable target.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,8 @@
 // swift-tools-version: 6.2
 import PackageDescription
 
-let package = Package(name: "brainf*ck compiler", defaultLocalization: "en-GB")
+let package = Package(
+    name: "brainf*ck compiler",
+    defaultLocalization: "en-GB",
+    targets: [.executableTarget(name: "brainf*ck compiler")]
+)
