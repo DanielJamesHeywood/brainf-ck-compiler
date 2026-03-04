7476bee465323217460d0c8082b27844946a20fc
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-04 22:16:31 +0000

    Testing initialization of command

--- Sources/BFCommand tests/Command tests.swift
+++ Sources/BFCommand tests/Command tests.swift
@@ -1,4 +1,37 @@
 import BFCommand
 import Testing
 
-@Suite struct CommandTests {}
+@Suite struct CommandTests {
+    
+    @Test func initializingCommandToIncrementPointer() {
+        #expect(Command(">") == .incrementPointer)
+    }
+    
+    @Test func initializingCommandToDecrementPointer() {
+        #expect(Command("<") == .decrementPointer)
+    }
+    
+    @Test func initializingCommandToIncrementByte() {
+        #expect(Command("+") == .incrementByte)
+    }
+    
+    @Test func initializingCommandToDecrementByte() {
+        #expect(Command("-") == .decrementByte)
+    }
+    
+    @Test func initializingCommandToOutputByte() {
+        #expect(Command(".") == .outputByte)
+    }
+    
+    @Test func initializingCommandToInputByte() {
+        #expect(Command(",") == .inputByte)
+    }
+    
+    @Test func initializingCommandToStartLoop() {
+        #expect(Command("[") == .startLoop)
+    }
+    
+    @Test func initializingCommandToEndLoop() {
+        #expect(Command("]") == .endLoop)
+    }
+}
