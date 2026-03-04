fe38f570b0a51fc62a122a4bc22ed70b92b7e7ed
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-04 21:16:18 +0000

    Unindent print statements in main

--- Sources/brainf*ck compiler/main.swift
+++ Sources/brainf*ck compiler/main.swift
@@ -3,35 +3,23 @@ import Utilities
 
 do {
     guard CommandLine.arguments.count == 2 else {
-        print(
-            "Expected exactly 1 argument, but got \(CommandLine.arguments.count - 1)",
-            to: .standardError
-        )
+        print("Expected exactly 1 argument, but got \(CommandLine.arguments.count - 1)", to: .standardError)
         exit(with: .failure)
     }
     let bfFilePath = FilePath(CommandLine.arguments[1])
     guard let bfFileExtension = bfFilePath.extension else {
-        print(
-            "Expected a path to a brainf*ck file, but got a path to a file with no extension",
-            to: .standardError
-        )
+        print("Expected a path to a brainf*ck file, but got a path to a file with no extension", to: .standardError)
         exit(with: .failure)
     }
     guard bfFileExtension == "bf" else {
-        print(
-            "Expected a path to a brainf*ck file, but got a path to a file with extension '.\(bfFileExtension)'",
-            to: .standardError
-        )
+        print("Expected a path to a brainf*ck file, but got a path to a file with extension '.\(bfFileExtension)'", to: .standardError)
         exit(with: .failure)
     }
     let contentsOfBFFile: String
     do {
         contentsOfBFFile = try String(utf8ContentsOfFileAt: bfFilePath)
     } catch {
-        print(
-            "Failed to read from '\(bfFilePath)': \(error)",
-            to: .standardError
-        )
+        print("Failed to read from '\(bfFilePath)': \(error)", to: .standardError)
         exit(with: .failure)
     }
     exit(with: .failure)
