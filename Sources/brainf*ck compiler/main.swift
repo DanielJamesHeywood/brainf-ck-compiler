1ff03ce0a70107aecd24be366f54b3eaa1f20d84
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-04 21:06:00 +0000

    Initialize a file-path from the command line argument and check it's extension is valid

--- Sources/brainf*ck compiler/main.swift
+++ Sources/brainf*ck compiler/main.swift
@@ -1,10 +1,28 @@
+import System
 import Utilities
 
-guard CommandLine.arguments.count == 2 else {
-    print(
-        "Expected exactly 1 argument, but got \(CommandLine.arguments.count - 1)",
-        to: .standardError
-    )
+do {
+    guard CommandLine.arguments.count == 2 else {
+        print(
+            "Expected exactly 1 argument, but got \(CommandLine.arguments.count - 1)",
+            to: .standardError
+        )
+        exit(with: .failure)
+    }
+    let bfFilePath = FilePath(CommandLine.arguments[1])
+    guard let bfFileExtension = bfFilePath.extension else {
+        print(
+            "Expected a path to a brainf*ck file, but got a path to a file with no extension",
+            to: .standardError
+        )
+        exit(with: .failure)
+    }
+    guard bfFileExtension == "bf" else {
+        print(
+            "Expected a path to a brainf*ck file, but got a path to a file with extension '.\(bfFileExtension)'",
+            to: .standardError
+        )
+        exit(with: .failure)
+    }
     exit(with: .failure)
 }
-exit(with: .failure)
