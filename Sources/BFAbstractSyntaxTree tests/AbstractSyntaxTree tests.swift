86073a2e9baf47473e32ad06a8bb1d41916d1309
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-05 00:14:02 +0000

    Test for unmatched start and end loops

--- Sources/BFAbstractSyntaxTree tests/AbstractSyntaxTree tests.swift
+++ Sources/BFAbstractSyntaxTree tests/AbstractSyntaxTree tests.swift
@@ -1,1 +1,23 @@
+import BFAbstractSyntaxTree
+import Testing
 
+@Suite struct AbstractSyntaxTreeTests {
+    
+    @Test func initializingAbstractSyntaxTreeWithUnmatchedStartLoop() {
+        #expect(
+            throws: AbstractSyntaxTree.InitializationError.unmatchedStartLoop,
+            performing: {
+                try AbstractSyntaxTree([.startLoop])
+            }
+        )
+    }
+    
+    @Test func initializingAbstractSyntaxTreeWithUnmatchedEndLoop() {
+        #expect(
+            throws: AbstractSyntaxTree.InitializationError.unmatchedEndLoop,
+            performing: {
+                try AbstractSyntaxTree([.endLoop])
+            }
+        )
+    }
+}
