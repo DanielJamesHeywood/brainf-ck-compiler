6a4e06d5218f6f32f216076f24013e1c8c37a03f
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-05 01:03:03 +0000

    Test initializing a AST from different commands

--- Sources/BFAbstractSyntaxTree tests/AbstractSyntaxTree tests.swift
+++ Sources/BFAbstractSyntaxTree tests/AbstractSyntaxTree tests.swift
@@ -3,6 +3,30 @@ import Testing
 
 @Suite struct AbstractSyntaxTreeTests {
     
+    @Test func initializingAbstractSyntaxTreeFromIncrementPointer() throws {
+        try #expect(AbstractSyntaxTree([.incrementPointer]).root == [.incrementPointer])
+    }
+    
+    @Test func initializingAbstractSyntaxTreeFromDecrementPointer() throws {
+        try #expect(AbstractSyntaxTree([.decrementPointer]).root == [.decrementPointer])
+    }
+    
+    @Test func initializingAbstractSyntaxTreeFromIncrementByte() throws {
+        try #expect(AbstractSyntaxTree([.incrementByte]).root == [.incrementByte])
+    }
+    
+    @Test func initializingAbstractSyntaxTreeFromDecrementByte() throws {
+        try #expect(AbstractSyntaxTree([.decrementByte]).root == [.decrementByte])
+    }
+    
+    @Test func initializingAbstractSyntaxTreeFromOutputByte() throws {
+        try #expect(AbstractSyntaxTree([.outputByte]).root == [.outputByte])
+    }
+    
+    @Test func initializingAbstractSyntaxTreeFromInputByte() throws {
+        try #expect(AbstractSyntaxTree([.inputByte]).root == [.inputByte])
+    }
+    
     @Test func initializingAbstractSyntaxTreeWithLoop() throws {
         try #expect(AbstractSyntaxTree([.startLoop, .endLoop]).root == [.loop([])])
     }
