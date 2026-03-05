ffac50c7edf184bd4887cc5e9dc9cc68574f4e65
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-05 00:59:22 +0000

    Testing for initializing an abstract syntax tree with a loop

--- Sources/BFAbstractSyntaxTree tests/AbstractSyntaxTree tests.swift
+++ Sources/BFAbstractSyntaxTree tests/AbstractSyntaxTree tests.swift
@@ -3,6 +3,10 @@ import Testing
 
 @Suite struct AbstractSyntaxTreeTests {
     
+    @Test func initializingAbstractSyntaxTreeWithLoop() throws {
+        try #expect(AbstractSyntaxTree([.startLoop, .endLoop]).root == [.loop([])])
+    }
+    
     @Test func initializingAbstractSyntaxTreeWithUnmatchedStartLoop() {
         #expect(
             throws: AbstractSyntaxTree.InitializationError.unmatchedStartLoop,
