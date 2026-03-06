ff41ef7968bedfc859016e1181f66e7804e3b9cf
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-06 14:59:13 +0000

    Declare method for building subtract instructions

--- Sources/LLVMSwift/LLVMBuilder.swift
+++ Sources/LLVMSwift/LLVMBuilder.swift
@@ -18,4 +18,8 @@ extension LLVMBuilder {
     @inlinable public func buildAdd(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String) -> LLVMInt8 {
         LLVMInt8(value: LLVMBuildAdd(builder, lhs.value, rhs.value, name))
     }
+    
+    @inlinable public func buildSubtract(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String) -> LLVMInt8 {
+        LLVMInt8(value: LLVMBuildSub(builder, lhs.value, rhs.value, name))
+    }
 }
