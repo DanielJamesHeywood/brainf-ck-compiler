de17b63127b6ed2ca637be707d9400365806b8dc
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-06 21:30:57 +0000

    Changed function name to align them with llvm-c

--- Sources/LLVMSwift/LLVMBuilder.swift
+++ Sources/LLVMSwift/LLVMBuilder.swift
@@ -19,7 +19,7 @@ extension LLVMBuilder {
         LLVMInt8(value: LLVMBuildAdd(builder, lhs.value, rhs.value, name))
     }
     
-    @inlinable public func buildSubtract(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String = "") -> LLVMInt8 {
+    @inlinable public func buildSub(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String = "") -> LLVMInt8 {
         LLVMInt8(value: LLVMBuildSub(builder, lhs.value, rhs.value, name))
     }
     
