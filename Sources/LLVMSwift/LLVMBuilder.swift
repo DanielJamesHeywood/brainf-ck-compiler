174ac7303ff40656834aab43774f986cc0c7a67f
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-06 15:26:42 +0000

    Declare method for loading Int8 from a pointer

--- Sources/LLVMSwift/LLVMBuilder.swift
+++ Sources/LLVMSwift/LLVMBuilder.swift
@@ -15,15 +15,19 @@ public class LLVMBuilder {
 
 extension LLVMBuilder {
     
-    @inlinable public func buildStore(_ value: LLVMInt8, to pointer: LLVMInt8Pointer) {
-        LLVMBuildStore(builder, value.value, pointer.value)
-    }
-    
     @inlinable public func buildAdd(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String) -> LLVMInt8 {
         LLVMInt8(value: LLVMBuildAdd(builder, lhs.value, rhs.value, name))
     }
     
     @inlinable public func buildSubtract(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String) -> LLVMInt8 {
         LLVMInt8(value: LLVMBuildSub(builder, lhs.value, rhs.value, name))
+    }
+    
+    @inlinable public func buildLoad(_ type: LLVMInt8Type, from pointer: LLVMInt8Pointer, name: String) -> LLVMInt8 {
+        LLVMInt8(value: LLVMBuildLoad2(builder, type.type, pointer.value, name))
+    }
+    
+    @inlinable public func buildStore(_ value: LLVMInt8, to pointer: LLVMInt8Pointer) {
+        LLVMBuildStore(builder, value.value, pointer.value)
     }
 }
