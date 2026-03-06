29dd47dd0e923168dae2615862eb6b8058e170cd
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-06 14:56:09 +0000

    Declare method for building add instructions

--- Sources/LLVMSwift/LLVMBuilder.swift
+++ Sources/LLVMSwift/LLVMBuilder.swift
@@ -12,3 +12,10 @@ public class LLVMBuilder {
         LLVMDisposeBuilder(builder)
     }
 }
+
+extension LLVMBuilder {
+    
+    @inlinable public func buildAdd(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String) -> LLVMInt8 {
+        LLVMInt8(value: LLVMBuildAdd(builder, lhs.value, rhs.value, name))
+    }
+}
