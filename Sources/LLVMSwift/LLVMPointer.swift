f04a7c6e120eda3cd8d25f12540ba7ec3ba1981c
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-06 19:16:45 +0000

    Move AddressSpace to the LLVMType scope

--- Sources/LLVMSwift/LLVMPointer.swift
+++ Sources/LLVMSwift/LLVMPointer.swift
@@ -2,11 +2,14 @@ import LLVM
 
 public class LLVMPointer<Pointee: LLVMValue>: LLVMValue {}
 
-extension LLVMPointer {
+extension LLVMPointer.LLVMType {
     
     public typealias AddressSpace = UInt32
+}
+
+extension LLVMPointer {
     
-    @inlinable public static func makeType(pointeeType: Pointee.LLVMType, addressSpace: AddressSpace = 0) -> LLVMType {
+    @inlinable public static func makeType(pointeeType: Pointee.LLVMType, addressSpace: LLVMType.AddressSpace = 0) -> LLVMType {
         LLVMType(type: LLVMPointerType(pointeeType.type, addressSpace))
     }
 }
