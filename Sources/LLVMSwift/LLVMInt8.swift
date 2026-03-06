6f39d6c6078ebeed49b820a5e0dff567c2d626b1
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-06 20:25:05 +0000

    Simplified logic handling LLVMBool

--- Sources/LLVMSwift/LLVMInt8.swift
+++ Sources/LLVMSwift/LLVMInt8.swift
@@ -3,7 +3,7 @@ import LLVM
 public class LLVMInt8: LLVMValue {
     
     @inlinable public convenience init(_ value: UInt8, type: LLVMInt8Type) {
-        self.init(value: LLVMConstInt(type.type, UInt64(value), LLVMBool(false)))
+        self.init(value: LLVMConstInt(type.type, UInt64(value), 0))
     }
     
     @inlinable public convenience init(_ value: Int8, type: LLVMInt8Type) {
--- Sources/LLVMSwift/initializeLLVMBoolFromBool.swift
+++
@@ -1,8 +0,0 @@
-import LLVM
-
-extension LLVMBool {
-    
-    @inlinable init(_ value: Bool) {
-        self = value ? 1 : 0
-    }
-}
