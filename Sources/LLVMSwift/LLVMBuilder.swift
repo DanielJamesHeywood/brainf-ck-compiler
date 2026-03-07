b7f7bd51a111db0ebfc2b11490a971234f733586
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-07 01:40:40 +0000

    Standardized build function indentation

--- Sources/LLVMSwift/LLVMBuilder.swift
+++ Sources/LLVMSwift/LLVMBuilder.swift
@@ -15,19 +15,32 @@ public class LLVMBuilder {
 
 extension LLVMBuilder {
     
-    @inlinable public func buildAdd(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String = "") -> LLVMInt8 {
+    @inlinable public func buildAdd(
+        _ lhs: LLVMInt8, _ rhs: LLVMInt8,
+        name: String = ""
+    ) -> LLVMInt8 {
         LLVMInt8(value: LLVMBuildAdd(builder, lhs.value, rhs.value, name))
     }
     
-    @inlinable public func buildSubtract(_ lhs: LLVMInt8, _ rhs: LLVMInt8, name: String = "") -> LLVMInt8 {
+    @inlinable public func buildSubtract(
+        _ lhs: LLVMInt8, _ rhs: LLVMInt8,
+        name: String = ""
+    ) -> LLVMInt8 {
         LLVMInt8(value: LLVMBuildSub(builder, lhs.value, rhs.value, name))
     }
     
-    @inlinable public func buildLoad<Value: LLVMValue>(_ type: LLVMType<Value>, from pointer: LLVMPointer<Value>, name: String = "") -> Value {
+    @inlinable public func buildLoad<Value: LLVMValue>(
+        _ type: LLVMType<Value>,
+        from pointer: LLVMPointer<Value>,
+        name: String = ""
+    ) -> Value {
         Value(value: LLVMBuildLoad2(builder, type.type, pointer.value, name))
     }
     
-    @inlinable public func buildStore<Value: LLVMValue>(_ value: Value, to pointer: LLVMPointer<Value>) {
+    @inlinable public func buildStore<Value: LLVMValue>(
+        _ value: Value,
+        to pointer: LLVMPointer<Value>
+    ) {
         LLVMBuildStore(builder, value.value, pointer.value)
     }
     
