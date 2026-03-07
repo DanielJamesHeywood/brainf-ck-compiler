fb6ec266ad7d49ae5286b68e3e5ea148259ee392
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-07 21:52:05 +0000

    Remove void-type

--- Sources/LLVMSwift/LLVMVoidType.swift
+++
@@ -1,8 +0,0 @@
-import LLVM
-
-public class LLVMVoidType: LLVMType<LLVMVoid> {
-    
-    @inlinable convenience init(context: LLVMContext) {
-        self.init(type: LLVMVoidTypeInContext(context.context))
-    }
-}
