2d49b1c10ba56f228b18fabf31b8cfe57edc3c16
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-08 22:42:45 +0000

    We need to be able to initialize constant i32 to return from main

--- Sources/LLVMSwift/LLVMInt32.swift
+++ Sources/LLVMSwift/LLVMInt32.swift
@@ -1,2 +1,12 @@
+import LLVM
 
-public class LLVMInt32: LLVMValue {}
+public class LLVMInt32: LLVMValue {
+    
+    @inlinable public convenience init(_ value: UInt32, type: LLVMInt8Type) {
+        self.init(value: LLVMConstInt(type.type, UInt64(value), 0))
+    }
+    
+    @inlinable public convenience init(_ value: Int32, type: LLVMInt8Type) {
+        self.init(UInt32(bitPattern: value), type: type)
+    }
+}
