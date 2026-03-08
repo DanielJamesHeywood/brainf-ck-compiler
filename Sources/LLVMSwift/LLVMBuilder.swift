307a6f8ff132bb3b43d7cd7d0e5313f8dfc1b805
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-08 17:27:31 +0000

    Declare method for building icmp instructions

--- Sources/LLVMSwift/LLVMBuilder.swift
+++ Sources/LLVMSwift/LLVMBuilder.swift
@@ -60,6 +60,48 @@ extension LLVMBuilder {
         LLVMInt32(value: LLVMBuildZExt(builder, value.value, type.type, name))
     }
     
+    public enum Predicate {
+        case equalTo
+        case notEqualTo
+        case unsignedGreaterThan
+        case unsignedGreaterThanOrEqualTo
+        case unsignedLessThan
+        case unsignedLessThanOrEqualTo
+        case signedGreaterThan
+        case signedGreaterThanOrEqualTo
+        case signedLessThan
+        case signedLessThanOrEqualTo
+    }
+    
+    @inlinable public func buildCompare(
+        using predicate: Predicate,
+        _ lhs: LLVMInt8, _ rhs: LLVMInt8,
+        name: String = ""
+    ) -> LLVMInt1 {
+        switch predicate {
+        case .equalTo:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntEQ, lhs.value, rhs.value, name))
+        case .notEqualTo:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntNE, lhs.value, rhs.value, name))
+        case .unsignedGreaterThan:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntUGT, lhs.value, rhs.value, name))
+        case .unsignedGreaterThanOrEqualTo:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntUGE, lhs.value, rhs.value, name))
+        case .unsignedLessThan:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntULT, lhs.value, rhs.value, name))
+        case .unsignedLessThanOrEqualTo:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntULE, lhs.value, rhs.value, name))
+        case .signedGreaterThan:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntSGT, lhs.value, rhs.value, name))
+        case .signedGreaterThanOrEqualTo:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntSGE, lhs.value, rhs.value, name))
+        case .signedLessThan:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntSLT, lhs.value, rhs.value, name))
+        case .signedLessThanOrEqualTo:
+            return LLVMInt1(value: LLVMBuildICmp(builder, LLVMIntSLE, lhs.value, rhs.value, name))
+        }
+    }
+    
     @inlinable @discardableResult public func buildCall<Return: LLVMValue, each Argument: LLVMValue>(
         returnType: LLVMType<Return>,
         function: LLVMFunction<Return, repeat each Argument>,
