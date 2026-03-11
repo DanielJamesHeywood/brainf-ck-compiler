2a59a7d55f1fed36c4b90e9a13e30a10b7194bfa
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-11 04:10:14 +0000

    Fix bug in initializer caused by opaque pointer coersion

--- Sources/LLVMSwift/LLVMFunctionType.swift
+++ Sources/LLVMSwift/LLVMFunctionType.swift
@@ -2,14 +2,14 @@ import LLVM
 
 public class LLVMFunctionType<Return: LLVMValue, each Parameter: LLVMValue>: LLVMType<LLVMFunction<Return, repeat each Parameter>> {
     
-    @inlinable public convenience init(returnType: Return, parameterTypes: repeat LLVMType<each Parameter>) {
+    @inlinable public convenience init(returnType: LLVMType<Return>, parameterTypes: repeat LLVMType<each Parameter>) {
         var rawParameterTypes = [] as [LLVMTypeRef?]
         for parameterType in repeat each parameterTypes {
             rawParameterTypes.append(parameterType.rawType)
         }
         self.init(
             rawType: rawParameterTypes.withUnsafeMutableBufferPointer { buffer in
-                LLVM.LLVMFunctionType(returnType.rawValue, buffer.baseAddress, UInt32(buffer.count), 0)
+                LLVM.LLVMFunctionType(returnType.rawType, buffer.baseAddress, UInt32(buffer.count), 0)
             }
         )
     }
