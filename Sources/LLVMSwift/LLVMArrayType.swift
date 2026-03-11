05223016edfdc5236c66755a35ad4fe9af7a18fb
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-11 23:44:02 +0000

    Provide initializer for LLVMArrayType

--- Sources/LLVMSwift/LLVMArrayType.swift
+++ Sources/LLVMSwift/LLVMArrayType.swift
@@ -1,2 +1,11 @@
+import LLVM
 
-public class LLVMArrayType<Element: LLVMValue>: LLVMType<LLVMArray<Element>> {}
+public class LLVMArrayType<Element: LLVMValue>: LLVMType<LLVMArray<Element>> {
+    
+    public typealias ElementCount = Int
+    
+    @inlinable public convenience init(elementType: LLVMType<Element>, elementCount: ElementCount) {
+        self.init(rawType: LLVMArrayType2(elementType.rawType, UInt64(elementCount)))
+    }
+}
+
