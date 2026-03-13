f2821e245601ba5adcc02d11da542b5f0c22fc10
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-13 00:51:07 +0000

    Remove unused function initializer

--- Sources/LLVMSwift/LLVMFunction.swift
+++ Sources/LLVMSwift/LLVMFunction.swift
@@ -1,8 +1,2 @@
-import LLVM
 
-public class LLVMFunction<Return: LLVMValue, each Parameter: LLVMValue>: LLVMValue {
-    
-    @inlinable convenience init(module: LLVMModule, name: String = "", type: LLVMFunctionType<Return, repeat each Parameter>) {
-        self.init(rawValue: LLVMAddFunction(module.rawModule, name, type.rawType))
-    }
-}
+public class LLVMFunction<Return: LLVMValue, each Parameter: LLVMValue>: LLVMValue {}
