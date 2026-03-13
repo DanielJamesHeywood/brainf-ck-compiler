a2722c4961c1d050cfdde96544e1b715cdb9f137
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-13 00:47:47 +0000

    Refactor makeFunction

--- Sources/LLVMSwift/LLVMModule.swift
+++ Sources/LLVMSwift/LLVMModule.swift
@@ -15,11 +15,11 @@ public class LLVMModule {
 
 extension LLVMModule {
     
-    @inlinable public func makeFunction<Return: LLVMValue, each Parameter: LLVMValue>(
-        name: String = "",
-        type: LLVMFunctionType<Return, repeat each Parameter>
+    @inlinable public func addFunction<Return: LLVMValue, each Parameter: LLVMValue>(
+        type: LLVMFunctionType<Return, repeat each Parameter>,
+        name: String = ""
     ) -> LLVMFunction<Return, repeat each Parameter> {
-        LLVMFunction(module: self, name: name, type: type)
+        LLVMFunction(rawValue: LLVMAddFunction(rawModule, name, type.rawType))
     }
     
     public typealias AddressSpace = Int
