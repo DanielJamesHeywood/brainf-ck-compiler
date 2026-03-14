670bbbef4fdf4d8873b48ad3815d259fb41761b4
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-14 20:02:09 +0000

    Add method to append pre-existing basic-blocks to a function

--- Sources/LLVMSwift/LLVMFunction.swift
+++ Sources/LLVMSwift/LLVMFunction.swift
@@ -1,2 +1,10 @@
+import LLVM
 
 public class LLVMFunction<Return: LLVMValue, each Parameter: LLVMValue>: LLVMValue {}
+
+extension LLVMFunction {
+    
+    @inlinable public func appendBasicBlock(_ block: LLVMBasicBlock) {
+        LLVMAppendExistingBasicBlock(rawValue, block.rawBasicBlock)
+    }
+}
