4ae646d1e8f489eb1fa194f63b92fed4483c6018
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-15 22:38:10 +0000

    Fix api issues with llvm-swift

--- Sources/LLVMSwift/LLVMInt32.swift
+++ Sources/LLVMSwift/LLVMInt32.swift
@@ -2,11 +2,11 @@ import LLVM
 
 public class LLVMInt32: LLVMValue {
     
-    @inlinable public convenience init(_ value: UInt32, type: LLVMInt8Type) {
+    @inlinable public convenience init(_ value: UInt32, type: LLVMInt32Type) {
         self.init(rawValue: LLVMConstInt(type.rawType, UInt64(value), 0))
     }
     
-    @inlinable public convenience init(_ value: Int32, type: LLVMInt8Type) {
+    @inlinable public convenience init(_ value: Int32, type: LLVMInt32Type) {
         self.init(UInt32(bitPattern: value), type: type)
     }
 }
--- Sources/LLVMSwift/LLVMInt64.swift
+++ Sources/LLVMSwift/LLVMInt64.swift
@@ -2,11 +2,11 @@ import LLVM
 
 public class LLVMInt64: LLVMValue {
     
-    @inlinable public convenience init(_ value: UInt64, type: LLVMInt8Type) {
+    @inlinable public convenience init(_ value: UInt64, type: LLVMInt64Type) {
         self.init(rawValue: LLVMConstInt(type.rawType, UInt64(value), 0))
     }
     
-    @inlinable public convenience init(_ value: Int64, type: LLVMInt8Type) {
+    @inlinable public convenience init(_ value: Int64, type: LLVMInt64Type) {
         self.init(UInt64(bitPattern: value), type: type)
     }
 }
