4ead6630504bea83f8d48fc14819994e3edc3656
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-16 01:09:28 +0000

    Add wrapper over const-get-element-pointer

--- Sources/LLVMSwift/LLVMPointer.swift
+++ Sources/LLVMSwift/LLVMPointer.swift
@@ -1,2 +1,24 @@
+import LLVM
 
-public class LLVMPointer<Element: LLVMValue>: LLVMValue {}
+public class LLVMPointer<Element: LLVMValue>: LLVMValue {
+    
+    @inlinable public convenience init(
+        to type: LLVMType<Element>,
+        indexing array: LLVMArray<Element>,
+        at index: LLVMInt32,
+        noWrapFlags: [LLVMNoWrapFlag] = []
+    ) {
+        var rawIndex = index.rawValue as LLVMValueRef?
+        self.init(
+            rawValue: withUnsafeMutablePointer(to: &rawIndex) { pointerToRawIndex in
+                LLVMConstGEPWithNoWrapFlags(
+                    type.rawType,
+                    array.rawValue,
+                    pointerToRawIndex,
+                    1,
+                    noWrapFlags.reduce(0) { rawNoWrapFlags, noWrapFlag in rawNoWrapFlags | noWrapFlag.rawNoWrapFlag }
+                )
+            }
+        )
+    }
+}
