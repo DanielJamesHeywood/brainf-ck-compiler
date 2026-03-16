d9ffb78befde801468e127ecc5fcda63ce5ce4ea
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-16 13:23:18 +0000

    Prefer pointer type initializer to factory

--- Sources/LLVMSwift/LLVMPointerType.swift
+++ Sources/LLVMSwift/LLVMPointerType.swift
@@ -1,2 +1,8 @@
+import LLVM
 
-public class LLVMPointerType<Element: LLVMValue>: LLVMType<LLVMPointer<Element>> {}
+public class LLVMPointerType<Element: LLVMValue>: LLVMType<LLVMPointer<Element>> {
+    
+    @inlinable public convenience init(elementType: LLVMType<Element>, addressSpace: LLVMAddressSpace = 0) {
+        self.init(rawType: LLVM.LLVMPointerType(elementType.rawType, addressSpace))
+    }
+}
