eb9995a7c577363d61830630338b755e2a14053d
Daniel James Heywood <danieljamesheywood@icloud.com>
2026-03-20 18:50:20 +0000

    Switch from make-target to an initializer that takes a triple

--- Sources/LLVMSwift/LLVMTarget.swift
+++ Sources/LLVMSwift/LLVMTarget.swift
@@ -8,3 +8,15 @@ public class LLVMTarget {
         self.rawTarget = rawTarget
     }
 }
+
+extension LLVMTarget {
+    
+    @inlinable public convenience init(triple: LLVMTriple) throws(LLVMError) {
+        var rawTarget: LLVMTargetRef?
+        var rawErrorMessage: UnsafeMutablePointer<CChar>?
+        guard LLVMGetTargetFromTriple(triple.rawMessage, &rawTarget, &rawErrorMessage) == 0 else {
+            throw LLVMError(LLVMMessage(rawMessage: rawErrorMessage.unsafelyUnwrapped))
+        }
+        self.init(rawTarget: rawTarget.unsafelyUnwrapped)
+    }
+}
